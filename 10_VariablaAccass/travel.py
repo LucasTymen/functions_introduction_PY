372bbc9afc314af7a32a5282c1a085df7ec83349
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-14 21:54:11 +0200

    10_VariablaAccass

--- 10_VariablaAccass/travel.py
+++ 10_VariablaAccass/travel.py
@@ -1,6 +1,7 @@
 # This function will print a hardcoded count of how many locations we have.
+favorite_locations = "Paris, Norway, Iceland"
 def print_count_locations():
-  favorite_locations = "Paris, Norway, Iceland"
+  #favorite_locations = "Paris, Norway, Iceland"
   print("There are 3 locations")
 
 # This function will print the favorite locations
